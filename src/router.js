075592a450e452fb0d7b4fcb6442ddad3b8b8b34
Natali Likhachova <nataliya.likhachova@gmail.com>
2019-09-14 19:18:56 +0300

    Fix merge conflicts

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Index from "./views/Index";
-import Table from "./views/Table";
+import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -10,14 +9,18 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/index",
-      name: "index",
-      component: Index
+      path: "/",
+      name: "home",
+      component: Home
     },
     {
-      path: "/table",
-      name: "table",
-      component: Table
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
